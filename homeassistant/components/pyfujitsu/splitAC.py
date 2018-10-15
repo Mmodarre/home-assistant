c3813087d340fe432eb3435f68ac97c95393bd23
Mehdi Modarressi <memodarr@microsoft.com>
2018-10-16 01:15:25 +1100

    w

--- homeassistant/components/pyfujitsu/splitAC.py
+++ homeassistant/components/pyfujitsu/splitAC.py
@@ -172,6 +172,7 @@ class splitAC:
 
     @adjust_temperature.setter
     def adjust_temperature(self,properties):
+        print(properties)
         if isinstance(properties,(list, tuple)):
             self._adjust_temperature = self._get_prop_from_json('adjust_temperature',properties)
         elif isinstance(properties,int) or isinstance(properties,float):
