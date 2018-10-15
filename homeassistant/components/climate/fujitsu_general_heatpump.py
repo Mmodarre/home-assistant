24b27c521aa36fac7fcb9c2c9936410797ed3e00
Mehdi Modarressi <memodarr@microsoft.com>
2018-10-16 01:38:13 +1100

    www

--- homeassistant/components/climate/fujitsu_general_heatpump.py
+++ homeassistant/components/climate/fujitsu_general_heatpump.py
@@ -181,7 +181,7 @@ class FujitsuClimate(ClimateDevice):
     def set_temperature(self, **kwargs):
         """Set new target temperature."""
         #print (kwargs.get(ATTR_TEMPERATURE))
-        self._fujitsu_device.changeTemperature = kwargs.get(ATTR_TEMPERATURE)
+        self._fujitsu_device.changeTemperature(kwargs.get(ATTR_TEMPERATURE))
 
 ## up to here!
     def set_fan_mode(self, fan_mode):
